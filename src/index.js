fdf486db35f48dc536aea819211214eb4bc2e01b
Robin La Rondelle <robinlarondelle@hotmail.com>
2020-01-07 11:13:23 +0100

    Set windows wallpaper

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log("Hello World") 
+const settings = require("./config/settings");
+const fetch = require("node-fetch");
+global.fetch = fetch;
+const wallpaper = require("wallpaper");
+var fs = require("fs");
+let request = require("request");
+const Unsplash = require("unsplash-js").default;
+const { toJson } = require("unsplash-js");
+
+const unsplash = new Unsplash({
+  accessKey: settings.accessKey,
+  secret: settings.secretKey
+});
+
+unsplash.search
+  .photos("rotterdam", "1", "1", { orientation: "landscape" })
+  .then(toJson)
+  .then(res => {
+    console.log(res.results);
+
+    console.log(res.results[0].urls);
+    console.log(res.results[0].links);
+    download(res.results[0].urls.full, "download.jpg", () => {
+      (async () => {
+        await wallpaper.set("download.jpg");
+      })();
+    });
+  });
+
+let download = function(uri, filename, callback) {
+  request.head(uri, function(err, res, body) {
+    request(uri)
+      .pipe(fs.createWriteStream(filename))
+      .on("close", callback);
+  });
+};
