e4dddb24e08b50adb97e585b0ccff68402a913f1
Robin La Rondelle <robinlarondelle@hotmail.com>
2020-01-07 17:11:59 +0100

    Huge update

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,5 +1,40 @@
+const homedir = require("os").homedir()
+
 module.exports = {
-    "accessKey": "f2762ca3de935268b52a750778fc1c6ecdd398b0a16b0685403a586fc3f25dfa",
-    "secretKey": "2203eb870aa5f0059e6cc80a8b086a29695c9f5d4645e126c93471e81bcf12cc",
-    "redirectUrl": "urn:ietf:wg:oauth:2.0:oob"
-}
+  accessKey: "f2762ca3de935268b52a750778fc1c6ecdd398b0a16b0685403a586fc3f25dfa",
+  secretKey: "2203eb870aa5f0059e6cc80a8b086a29695c9f5d4645e126c93471e81bcf12cc",
+  redirectUrl: "urn:ietf:wg:oauth:2.0:oob",
+  saveImages: false,
+  questions: [
+    {
+      type: "input",
+      name: "category",
+      message: "What category do you want as wallpaper? ",
+      validate: function(value) {
+        if (!value) return "No category provided. Defaulted to Rotterdam.";
+        return true
+      },
+      default: "Rotterdam"
+    },
+    {
+      type: "confirm",
+      name: "saveImages",
+      message:
+        `Do you want the wallpaper to be saved on your computer? \nThey will be saved at '${homedir}\\Pictures\\Wallpaper Application'`,
+      default: true
+    },
+    {
+      type: "input",
+      name: "interval",
+      message:
+        "At what interval should the wallpaper be updated? \nFormat: '[number] [time-range: hour[s], day[s] etc]'",
+      validate: function(value) {
+        let result = /[1-9]{1,3} ((hour[s]*)|(day[s]*)|(week[s]*)|(month[s]*))/.test(
+          value
+        );
+        return result ? true : "Max range is 999 [time-range]. Please make sure you use the correct format";
+      },
+      default: "1 hour"
+    }
+  ]
+};
--- src/index.js
+++ src/index.js
@@ -1,36 +1,102 @@
 const settings = require("./config/settings");
 const fetch = require("node-fetch");
-global.fetch = fetch;
 const wallpaper = require("wallpaper");
-var fs = require("fs");
-let request = require("request");
+const fs = require("fs");
+const request = require("request");
 const Unsplash = require("unsplash-js").default;
 const { toJson } = require("unsplash-js");
+const os = require("os");
+const inquirer = require("inquirer");
+
+const homedir = os.homedir();
+let dir = `${homedir}\\Pictures\\Wallpaper Application`;
+if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+
+global.fetch = fetch;
 
 const unsplash = new Unsplash({
-  accessKey: settings.accessKey,
-  secret: settings.secretKey
+  accessKey: settings.accessKey
 });
 
-unsplash.search
-  .photos("rotterdam", "1", "1", { orientation: "landscape" })
-  .then(toJson)
-  .then(res => {
-    console.log(res.results);
+console.log(`
 
-    console.log(res.results[0].urls);
-    console.log(res.results[0].links);
-    download(res.results[0].urls.full, "download.jpg", () => {
-      (async () => {
-        await wallpaper.set("download.jpg");
-      })();
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+
+██╗    ██╗ █████╗ ██╗     ██╗     ██████╗  █████╗ ██████╗ ███████╗██████╗         
+██║    ██║██╔══██╗██║     ██║     ██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗        
+██║ █╗ ██║███████║██║     ██║     ██████╔╝███████║██████╔╝█████╗  ██████╔╝        
+██║███╗██║██╔══██║██║     ██║     ██╔═══╝ ██╔══██║██╔═══╝ ██╔══╝  ██╔══██╗        
+╚███╔███╔╝██║  ██║███████╗███████╗██║     ██║  ██║██║     ███████╗██║  ██║        
+ ╚══╝╚══╝ ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝        
+                                                                                  
+ █████╗ ██████╗ ██████╗ ██╗     ██╗ ██████╗ █████╗ ████████╗██╗ ██████╗ ███╗   ██╗
+██╔══██╗██╔══██╗██╔══██╗██║     ██║██╔════╝██╔══██╗╚══██╔══╝██║██╔═══██╗████╗  ██║
+███████║██████╔╝██████╔╝██║     ██║██║     ███████║   ██║   ██║██║   ██║██╔██╗ ██║
+██╔══██║██╔═══╝ ██╔═══╝ ██║     ██║██║     ██╔══██║   ██║   ██║██║   ██║██║╚██╗██║
+██║  ██║██║     ██║     ███████╗██║╚██████╗██║  ██║   ██║   ██║╚██████╔╝██║ ╚████║
+╚═╝  ╚═╝╚═╝     ╚═╝     ╚══════╝╚═╝ ╚═════╝╚═╝  ╚═╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
+                            
+
+* * * * * * * * * * *  * * By Robin La Rondelle * * * * * * * * * * * *  * 
+
+`);
+
+inquirer.prompt(settings.questions).then(awnswers => {
+  console.log(
+    `\n\n Searching for new wallpapers in the ${awnswers.category} category`
+  );
+
+  unsplash.photos
+    .getRandomPhoto({
+      query: `${awnswers.category}`,
+      orientation: "landscape",
+      count: 5
+    })
+    .then(toJson)
+    .then(res => {
+      res.forEach(result => {
+        if (result.width > 1920 && result.height > 1080) {
+
+          //HD-images only
+          downloadUnsplashImage(
+            result.urls.full,
+            `${dir}\\${awnswers.category}.jpg`,
+            filename => {
+              setWindowsWallpaper(filename);
+            }
+          );
+        }
+      });
     });
-  });
+});
 
-let download = function(uri, filename, callback) {
+/**
+ * Downloads an image from the Unsplash API and saves it to the C:\[USER]\images\Wallpaper Application folder
+ * @param {string} uri the uri from Unsplash to download the image from. Remember - only use the full resolution link
+ * @param {string} filename the desired file name to save to. If the file name already exists, it will be appended with a number
+ * @param {function} callback callback function which returns the filename the image is saved to
+ */
+let downloadUnsplashImage = function(uri, filename, callback) {
   request.head(uri, function(err, res, body) {
     request(uri)
-      .pipe(fs.createWriteStream(filename))
+      .pipe(writeFile(filename))
       .on("close", callback);
   });
+};
+
+let writeFile = function(filename) {
+  let folders = filename.split("\\");
+  folders.splice(-1,1)
+  folders = folders.join().replace(/,/g, "/")
+  fs.readdir(folders, (err, files) => {
+    console.log(files);
+    fs.createWriteStream(filename, { flags: "a" });
+  });
+}
+
+let setWindowsWallpaper = function(url) {
+  (async () => {
+    await wallpaper.set(url);
+    console.log("successfully set new wallpaper");
+  })();
 };
